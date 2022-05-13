1d0782b806ae8650bd9ff2f56a6bf8b3d1d6791a
lortega <luisangelortegasanpedro@gmail.com>
2022-05-13 18:08:58 +0200

    Instalador

--- UI/DashBoard_UI/Informes.cs
+++ UI/DashBoard_UI/Informes.cs
@@ -34,16 +34,11 @@ namespace DashBoard_UI
             }
 
             ReportDocument cryRpt = new ReportDocument();
-            //
-            //string path = this.informe;
-            //string path = "../../Inf4VentasTotalesPorClientes.rpt";
             cryRpt.Load(path);
 
             //cryRpt.SetDataSource("Data Source=192.168.0.19;Initial Catalog=Northwind;Persist Security Info=True;User ID=lortega;Password=P@ssw0rd");
-
             //cryRpt.SetDatabaseLogon("lortega", "P@ssw0rd", @"localhost", "Northwind");
-            //cryRpt.SetParameterValue("txtNombreCli", _datos.sNombre);
-            //cryRpt.SetParameterValue("txtNIFCli", _datos.sNIF);
+           
             this.crystalReportViewer1.ReportSource = cryRpt;
         }
     }
